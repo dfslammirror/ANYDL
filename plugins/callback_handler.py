13ef538cded3c60fe824fc98ccad7d025489a28f
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-12-07 12:09:54 +0530

    Update callback_handler.py

--- plugins/callback_handler.py
+++ plugins/callback_handler.py
@@ -5,6 +5,11 @@ from pyrogram.types import CallbackQuery
 from helper_funcs.display_progress import humanbytes
 #from plugins.youtube_dl_button import d_directory
 
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
 d_directory = Config.DOWNLOAD_LOCATION + '/' + str(update.message_id)
 smze = 0
 
